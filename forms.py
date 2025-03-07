d707664588f4979240a145e496b5db3a17ebc0b3
sp <143427973+SuperUser1245@users.noreply.github.com>
2025-03-07 14:39:43 +0200

    Create forms.py

---
+++ forms.py
@@ -0,0 +1,11 @@
+class ProjectForm(ModelForm):
+  class Meta:
+    model = Project
+    fields = ['title', 'description', author]
+
+  def __init__(self, *args, **kwargs):
+    super(ProjectForm, self).__init__(*args, **kwargs)
+
+    for name, field in self.fields.items():
+      field.widget.attrs.update({'class': 'input'})
+  
