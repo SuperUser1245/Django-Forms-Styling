f94e7e39b84fd46b86f6a389aa7d8d0f84e083b9
sp <143427973+SuperUser1245@users.noreply.github.com>
2025-03-07 14:40:36 +0200

    Update forms.py

--- forms.py
+++ forms.py
@@ -7,5 +7,5 @@ class ProjectForm(ModelForm):
     super(ProjectForm, self).__init__(*args, **kwargs)
 
     for name, field in self.fields.items():
-      field.widget.attrs.update({'class': 'input'})
+      field.widget.attrs.update({'class': 'input'})  #adding css atributes ex.  <input class="..."></input>
   
